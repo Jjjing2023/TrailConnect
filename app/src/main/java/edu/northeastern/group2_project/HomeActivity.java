9401a5f7ff42e4ec9b3df63bbbec10361f3a750b
shuyue6481 <zhang.shuyue1@northeastern.edu>
2025-07-29 01:34:34 -0400

    Add Post button

--- app/src/main/java/edu/northeastern/group2_project/HomeActivity.java
+++ app/src/main/java/edu/northeastern/group2_project/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -77,6 +78,13 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
+        });
+
+        // button to post a new event
+        ImageButton postButton = findViewById(R.id.btnPost);
+        postButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, CreateEventActivity.class);
+            startActivity(intent);
         });
     }
 
